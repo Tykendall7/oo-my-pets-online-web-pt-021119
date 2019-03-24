620d01bd0f1e2b5866726da823248d1bdb9de6ec
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 23:54:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,11 +44,11 @@ class Owner
   end
   
   def walk_dogs
-    
+    pets[:dogs].map {|dog| dog.mood = "happy"}
   end
   
   def play_with_cats
-    
+    pets[:cats].map {|cats| cat.mood = "happy"}
   end
   
   def feed_fish
