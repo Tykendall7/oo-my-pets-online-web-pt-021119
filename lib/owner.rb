0dcfaa4070f20ef94123c21f37d40fe57671b5ea
Tymeckia Kendall <tykend@gmail.com>
2019-03-24 23:22:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,11 @@
 class Owner
   # code goes here
-  attr_accessor :owner, :cat, :dog, :fish
+  attr_accessor :owner, :cat, :dog, :fish, :species
   attr_reader :name
   @@all=[]
+  @@count_returns = 0
+  @@reset_all= 0
+  
+  
+
 end
